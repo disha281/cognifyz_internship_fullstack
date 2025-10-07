6ae0448c7da3b53e042e718abcea0c178bcc5c1a
dish281 <disharasalkar28@gmail.com>
2025-10-07 13:17:19 +0530

    Task 4 completed

---
+++ Task4/alert.js
@@ -0,0 +1,25 @@
+document.getElementById("registrationForm").onsubmit = function (event) {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var btn = document.getElementById("btn");
+
+  function redalert() {
+    btn.style.backgroundColor = "red";
+    return;
+  }
+
+  if (email === "") {
+    alert("Please enter your email.");
+    event.preventDefault();
+    return;
+  }
+
+  if (!password.match(/[a-zA-Z]/) || !password.match(/[0-9]/)) {
+    btn.style.backgroundColor = "red";
+    setTimeout(function () {
+      btn.style.backgroundColor = "#ff818bff";
+    }, 3000);
+    event.preventDefault();
+    alert("Password must contain both letters and numbers.");
+  }
+};
